e3c69391ba9a0456c14631f9a8a64d684caa3b9b
dorianrobineau35 <dorianrobineau35@gmail.com>
2025-04-15 17:51:20 +0200

    Ajout du dossier frontend2 en tant que dossier classique

---
+++ frontend2/src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import DepartmentView from '@/views/DepartmentView.vue'
+import SearchView from '@/views/SearchView.vue'
+import EmployeeView from '@/views/EmployeeView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'departments',
+    component: DepartmentView
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: SearchView
+  },
+  {
+    path: '/employee/:id?',
+    name: 'employee',
+    component: EmployeeView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
